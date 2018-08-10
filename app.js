a925a0433c7fb702cea3be9c07b106eea583a37d
bunlong <bunlong.van@gmail.com>
2018-08-10 17:16:35 +0700

    Set upload images

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  console.log(err);
+  // res.render('error');
 });
 
 module.exports = app;
